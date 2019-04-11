b657a77d688a8feb3074207473aadcb550ac5209
kinopotato <kinoiskino@gmail.com>
2019-04-11 17:45:28 +0800

    added practice async function testing

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -6,10 +6,50 @@ const utils = require("./utils");
 it("should add two numbers", () => {
   let res = utils.add(33, 11);
   expect(res).toBe(44).toBeA("number");
-
 })
+
+it("should async add two numbers", (done) => {
+  utils.asyncAdd(4, 3, (sum) => {
+    expect(sum).toBe(7).toBeA("number");
+    done();
+  });
+});
 
 it("should square a number", () => {
   let res = utils.square(2);
   expect(res).toBe(4).toBeA("number");
 })
+
+it("should async square a number", (done) => {
+  utils.asyncSquare(3, (square) => {
+    expect(square).toBe(9).toBeA("number");
+    done();
+  })
+})
+
+it("should verify first and last names are set", () => {
+  let res = utils.setName({
+    age: 25,
+    location: "Philly"
+    }, "Andrew Mead");
+
+    expect(res).toInclude(
+      {firstName: "Andrew",
+       lastName: "Mead"}).toBeAn("object")
+})
+
+// it("should expect some values", () => {
+//   //toEqual is used to compare object "equality"
+//   expect({name: "Andrew"}).toEqual({name: "Andrew"});
+//   expect([2,3,4]).toInclude(4);
+//   expect([2,3,4]).toExclude(5);
+//   expect({
+//     name: "Andrew",
+//     age: 25,
+//     location: "Philly"
+//   }).toInclude({
+//     age: 25
+//   })
+// })
+
+
